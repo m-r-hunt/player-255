fabe3444b4d1b958f48513ab73553e6d00ba3d82
Max Hunt <max@huntw3.com>
2019-02-02 23:20:08 +0000

    Making some progress on site generation.

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to player-255
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- src/player_255/site_generator.clj
+++ src/player_255/site_generator.clj
@@ -1,6 +1,15 @@
 (ns player-255.site-generator
   (:require [selmer.parser :as template]))
 
+(defn generate-game-page
+  [game]
+  (if (:shortname game)
+    (spit (str "docs/" (:shortname game) ".html") (template/render-file "templates/game-page.html" game))))
+
 (defn generate
   [remaining-games played-games]
-  (template/render-file "templates/lists.html" {:remaining-games remaining-games}))
+  (clojure.java.io/make-parents "docs/foo.html")
+  (spit "docs/lists.html" (template/render-file "templates/lists.html" {:remaining-games remaining-games :played-games played-games}))
+  (map generate-game-page played-games)
+  ;; TODO Copy images to output
+  nil)
