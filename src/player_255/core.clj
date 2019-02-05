d69b5772b8a782cce81f59a568c8d6660e1abecb
Max Hunt <max@huntw3.com>
2019-02-05 20:12:17 +0000

    Wrote data entry code, started entry.

--- src/player_255/core.clj
+++ src/player_255/core.clj
@@ -30,8 +30,8 @@
   (read-line))
 
 (defn ask-status
-  []
-  (println "Status? (c)omplete (n)ot complete (o)ther")
+  [game]
+  (println (str "Status of " game " ? (c)omplete (n)ot complete (o)ther"))
   (case (first (read-line))
     \c [:status :complete]
     \n [:status :not-complete]
@@ -44,9 +44,9 @@
       (conj :completion-date (datestamp))
       (conj :shortname (ask "Enter shortname:"))
       (conj :notes (ask "Enter notes:"))
-      (concat (ask-status))))
+      (concat (ask-status game))))
 
-(defn -main
+(defn old-main
   "Get input about the last played game, pick the next one, and regenerate the site."
   [& args]
   (let [dry-run (some #(= "--dry-run" %) *command-line-args*)
@@ -79,3 +79,54 @@
   (let [csv-data (apply str (map #(str (:game %) "," (:rating %) "," (:meta-rating %) "\n")
                                  (butlast played-games)))]
     (spit "played-games.csv" csv-data)))
+
+(defn data-entry-shortname
+  [game]
+  (if (not (:shortname game))
+    (assoc game :shortname (ask (str "Enter shortname for " (:game game) ":")))
+    game))
+
+(defn data-entry-status
+  [game]
+  (if (not (:status game))
+    (apply assoc game (ask-status (:game game)))
+    game))
+
+(defn data-entry-notes
+  [game]
+  (if (not (:notes game))
+    (do
+      (println (str "Enter notes for " (:game game) ":"))
+      (loop [game game]
+        (let [input (read-line)]
+          (if (= input "dexit")
+            game
+            (recur (update game :notes str input "\n"))))))
+    game))
+
+(defn data-entry-date
+  [game]
+  (if (not (:completion-date game))
+    (assoc game :completion-date (ask (str "Enter completion date for " (:game game) ":")))
+    game))
+
+(defn data-entry
+  [game]
+  (-> game
+      (data-entry-shortname)
+      (data-entry-date)
+      (data-entry-status)
+      (data-entry-notes)))
+
+(defn -main
+  []
+  (loop [i 0 played-games (edn/read-string (slurp "played-games.edn"))]
+    (if (< i (count played-games))
+      (if (or (not (:shortname (nth played-games i)))
+              (not (:status (nth played-games i)))
+              (not (:notes (nth played-games i)))
+              (not (:completion-date (nth played-games i))))
+        (let [played-games (update played-games i data-entry)]
+          (spit "played-games.edn" (with-out-str (pp/pprint played-games)))
+          (recur (inc i) played-games))
+        (recur (inc i) played-games)))))
